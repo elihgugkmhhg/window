2858a53a0d92156541c917d4f02b6db9ae24d052
elihgugkmhhg <150719284+elihgugkmhhg@users.noreply.github.com>
2023-11-13 11:43:05 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,13 @@
+let sprite: game.LedSprite = null
+sprite.move(1)
+basic.showLeds(`
+    . . # . .
+    . # # # .
+    . # # # .
+    # # # # #
+    . . # . .
+    `)
+basic.showString("MERRY CHRISTMAS")
+basic.forever(function () {
+	
+})
